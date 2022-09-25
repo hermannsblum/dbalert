7d3c7468e69b0ddf3ab89d23485604402567b467
Hermann <blumh@ethz.ch>
2022-09-25 21:09:00 +0200

    ability for multiple station IDs in docker

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-crontab -l | { cat; echo "$DBALERT_SCHEDULE . /project_env.sh; python3 /dbalert.py > /proc/1/fd/1 2>/proc/1/fd/2"; } | crontab -
+# iterate through comma-separated list of schedule variables
+Backup_of_internal_field_separator=$IFS
+IFS=,
+for station in $DBALERT_STATIONIDS; do
+   crontab -l | { cat; echo "$DBALERT_SCHEDULE . /project_env.sh; python3 /dbalert.py --station-id $station > /proc/1/fd/1 2>/proc/1/fd/2"; } | crontab -
+done
+IFS=$Backup_of_internal_field_separator
+crontab -l
 printenv | grep -v DBALERT_SCHEDULE | sed 's/^\(.*\)$/export \1/g' > /project_env.sh
 crond -f
